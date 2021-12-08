885163c7d0ea8349933138ac34b2e5eeb62a286a
ShahidBangash <shahidbangashsi94@gmail.com>
2021-12-08 19:46:05 +0500

    Fixed issue with Fetch

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 var bodyParser = require("body-parser");
 // var fetch = require("node-fetch");
 // import fetch from 'node-fetch';
-const fetch = import('node-fetch');
+// const fetch = require('node-fetch');
+const axios = require('axios');
 const blockedPhrases = new RegExp(/porn|sexy/);
 
 app.use(express.json());
@@ -21,7 +22,7 @@ var corsOptions = {
 app.get("/", cors(corsOptions), function (req, res, next) {
     res.json({ msg: "This is CORS-enabled for Flutter Web" });
 });
-app.post("/proxy-request", cors(corsOptions), function (req, res, next) {
+app.post("/proxy-request", cors(corsOptions), async function(req, res, next)  {
 
     // let url = req.query.url;
     // if (!url) {
@@ -37,15 +38,28 @@ app.post("/proxy-request", cors(corsOptions), function (req, res, next) {
     if (url.match(blockedPhrases)) {
         res.status(403).send('Phrase in URL is disallowed.');
     }
-    fetch(url, {
-        method: req.method,
-        body: req.get('content-type') === 'application/json' ? JSON.stringify(req.body) : req.body,
-        headers: {
-            'Content-Type': req.get('Content-Type'),
-        },
-    })
-        .then(r => r.headers.get('content-type') === 'application/json' ? r.json() : r.text())
-        .then(body => res.status(200).send(body));
+
+    axios.post(url, req.body)
+      .then(function (response) {
+        console.log(response);
+        res.status(200).json({response});
+
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.status(400).json({success: false, error});
+      });
+
+    // axios.post(url, req.body).the;
+    // await axios(url, {
+    //     method: req.method,
+    //     body: req.get('content-type') === 'application/json' ? JSON.stringify(req.body) : req.body,
+    //     headers: {
+    //         'Content-Type': req.get('Content-Type'),
+    //     },
+    // })
+    //     .then(r => r.headers.get('content-type') === 'application/json' ? r.json() : r.text())
+    //     .then(body => res.status(200).send(body));
 });
 
 
