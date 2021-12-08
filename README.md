f03a1ccc0e1301de74ab30faea1f14fae0a67c10
ShahidBangash <shahidbangashsi94@gmail.com>
2021-12-08 18:46:31 +0500

    Added Proxy Code function

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proxy CORS for Flutter WEB
+
+For those who are facing problem with FLutter web .They can send request on this [URL]("https://allowcorsflutter.herokuapp.com/")
+
+i am simply using fetch to send request and then send respond back to caller. 
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 var bodyParser = require("body-parser");
+// var fetch = require("node-fetch");
+// import fetch from 'node-fetch';
+const fetch = import('node-fetch');
+const blockedPhrases = new RegExp(/porn|sexy/);
 
 
 var corsOptions = {
@@ -11,9 +15,40 @@ var corsOptions = {
 };
 
 app.get("/", cors(corsOptions), function (req, res, next) {
-    res.json({ msg: "This is CORS-enabled for only ears app." });
+    res.json({ msg: "This is CORS-enabled for Flutter Web" });
 });
-app.post("/", cors(corsOptions), function (req, res, next) {
-    res.json({ msg: "This is CORS-enabled for only ears app." });
+app.post("/proxy-request", cors(corsOptions), function (req, res, next) {
+
+    let url = req.query.url;
+
+    if (!url) {
+        url = req.body.url;
+    }
+    if (!url) {
+        res.status(403).send('URL is empty.');
+    }
+    console.log('Request:', url);
+    // disallow blocked phrases
+    if (url.match(blockedPhrases)) {
+        res.status(403).send('Phrase in URL is disallowed.');
+    }
+
+    fetch(url, {
+        method: req.method,
+        body: req.get('content-type') === 'application/json' ? JSON.stringify(req.body) : req.body,
+        headers: {
+            'Content-Type': req.get('Content-Type'),
+        },
+    })
+        .then(r => r.headers.get('content-type') === 'application/json' ? r.json() : r.text())
+        .then(body => res.status(200).send(body));
+});
+
+
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+    console.log(`Click on link to see http://localhost:${port}`);
 });
 
